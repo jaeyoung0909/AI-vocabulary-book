864e421a34edd335e5714e09cf93aec8ae614df3
Moonde <jaeyoung0909@kaist.ac.kr>
2019-07-09 15:31:17 -0700

    add frequency checker

---
+++ AI-voc/AI_voc/AI_voc_app/static/js/frequency.js
@@ -0,0 +1,17 @@
+var freq_checker = function wordFreq(string) {
+    var words = string.toLowerCase().replace(/[^A-z ]/g,'').split(/\s/);
+    var freqMap = {};
+    words.forEach(function(w) {
+        if (!freqMap[w]) {
+            freqMap[w] = 0;
+        }
+        freqMap[w] += 1;
+    });
+    var ordered_freqMap = [];
+    for (var w in freqMap) {
+        ordered_freqMap.push([w, freqMap[w]]);
+    }
+    return ordered_freqMap.sort(function(a, b) {
+        return a[1] - b[1];
+    });
+}
