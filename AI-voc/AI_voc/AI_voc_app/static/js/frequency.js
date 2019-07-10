ca30b82ee4ced7f9f14507c12d887ef4088e7068
Moonde <jaeyoung0909@kaist.ac.kr>
2019-07-10 13:54:47 -0700

    frequency check box

--- AI-voc/AI_voc/AI_voc_app/static/js/frequency.js
+++ AI-voc/AI_voc/AI_voc_app/static/js/frequency.js
@@ -1,5 +1,6 @@
-var freq_checker = function wordFreq(string) {
-    var words = string.toLowerCase().replace(/[^A-z ]/g,'').split(/\s/);
+var freqChecker = function wordFreq(string) {
+    var words = string.toLowerCase().replace(/[^A-z ]/g,' ').split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
+    
     var freqMap = {};
     words.forEach(function(w) {
         if (!freqMap[w]) {
@@ -14,5 +15,27 @@ var freq_checker = function wordFreq(string) {
     return ordered_freqMap.sort(function(a, b) {
         return a[1] - b[1];
     });
+};
+
+var showList = function show() {
+    var string = document.getElementById('user_text').value;
+    var ordered = freqChecker(string);
+    console.log(ordered);
+    for (var w in ordered) {
+
+        var elem = document.createElement('p');
+        var word = document.createTextNode(ordered[w][0]);
+        var input = document.createElement('INPUT');
+        input.setAttribute ('type', 'checkbox');
+        input.setAttribute ('class', 'known_checker');
+
+        elem.appendChild (word);
+        elem.appendChild (input);
+        document.getElementById('list').appendChild (elem); 
+    }
+};
+
+var myFunction = function (){ 
+    console.log("hello, world! dfdfdf") 
 };
 
