f3747ddc4099215cabc7d9291d0d6231a44c730f
parksihwan <psh150204@kaist.ac.kr>
2019-07-18 17:07:23 -0700

    Create python program for SVD.

--- SVD/svd.py
+++ SVD/svd.py
@@ -1,1 +1,12 @@
 import numpy as np
+from numpy import genfromtxt
+import sys
+
+# Get the matrix from given filename.
+M = genfromtxt(sys.argv[1], delimiter=',')
+
+U, S, V = np.linalg.svd(M, full_matrices=False)
+M_h = np.matmul(np.matmul(U, np.diag(S)), V)
+print(np.std(M), np.std(M_h), np.std(M - M_h))
+
+np.savetxt('output.csv', M_h, delimiter=',')
